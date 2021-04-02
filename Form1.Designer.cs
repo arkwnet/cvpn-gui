e90c123383f1bff044dc8c15408c7dad1dce7a04
Sora Arakawa <sr@arkw.jp>
2021-04-02 23:00:35 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace cvpn_gui
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.Size = new System.Drawing.Size(784, 441);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 441);
+			this.Controls.Add(this.dataGridView1);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "cvpn-gui";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text;
+
+namespace cvpn_gui
+{
+
+	public partial class Form1 : Form
+	{
+		string AppName = "cvpn-gui";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.Text = AppName;
+			dataGridView1.ColumnCount = 4;
+			dataGridView1.Columns[0].HeaderText = "名前";
+			dataGridView1.Columns[1].HeaderText = "種類";
+			dataGridView1.Columns[2].HeaderText = "サイズ";
+			dataGridView1.Columns[3].HeaderText = "更新日時";
+			dataGridView1.Columns[0].Width = 300;
+			dataGridView1.Columns[3].Width = 200;
+		}
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			AddList(VPNGetList("/"));
+		}
+
+		private string VPNGetList(string path)
+		{
+			this.Text = path + " の一覧を取得中...  - "+AppName;
+			string command = Directory.GetCurrentDirectory() + "\\cvpn.exe";
+			ProcessStartInfo psInfo = new ProcessStartInfo();
+			psInfo.FileName = command;
+			psInfo.CreateNoWindow = true;
+			psInfo.UseShellExecute = false;
+			psInfo.RedirectStandardOutput = true;
+			psInfo.Arguments = "ls "+path;
+			psInfo.StandardOutputEncoding = Encoding.UTF8;
+			Process p = Process.Start(psInfo);
+			string output = p.StandardOutput.ReadToEnd();
+			output = output.Replace("\r\r\n", "\n");
+			this.Text = AppName;
+			return output;
+		}
+
+		private void AddList(string data)
+		{
+			StringReader rs = new StringReader(data);
+			while (rs.Peek() > -1)
+			{
+				string[] arr = rs.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+				if (arr[0] == "-")
+				{
+					dataGridView1.Rows.Add(CreateFileName(arr, 7), "フォルダ", arr[0], arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
+				} else if (arr[0] != "") {
+					dataGridView1.Rows.Add(CreateFileName(arr, 8), "ファイル", arr[0] + " "+ arr[1], arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5] + " " + arr[6]);
+				}
+				
+			}
+		}
+
+		private string CreateFileName(string[] arr, int i)
+		{
+			string fn = "";
+			while (i < arr.Length)
+			{
+				fn = fn + arr[i];
+				i++;
+				if (i < arr.Length)
+				{
+					fn = fn + " ";
+				}
+			}
+			return fn;
+		}
+	}
+}
